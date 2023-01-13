4c447fbdcd505fbb7ee928e249a0916d0304108b
Tudulabs <Steven.junio91@gmail.com>
2023-01-13 15:32:44 -0800

    1 file modified

--- src/pixi.ts
+++ src/pixi.ts
@@ -3,7 +3,7 @@ import * as PIXI from 'pixi.js';
 const pixiContainer = <any>document.getElementById(`app`);
 
 console.log(PIXI);
-console.log(`test`);
+console.log(`testasdsadsa`);
 let app = new PIXI.Application({
   width: 500,
   height: 500,

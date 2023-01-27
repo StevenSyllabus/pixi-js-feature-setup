49ac41920b708b3010c73dfbbb6247460e5e9302
Steven Junio <Steven.junio91@gmail.com>
2023-01-26 19:40:23 -0800

    Add tsnocheck for building

--- src/test-data.ts
+++ src/test-data.ts
@@ -1,6 +1,8 @@
 ///START CHRIS Test Data
 //var DAS = properties.das;
-var DAS = new Array({
+//@ts-nocheck
+var DAS = new Array(
+  {
     "Corner Roundness": 5,
     "Offset of top (y)": 395.96128953041926,
     "Syllabus Canvas Width": 483.328125,
@@ -14,16 +16,17 @@ var DAS = new Array({
     "Mobile Screenshot": "1674151421044x594933091784470800",
     "Box Width 250": 88,
     "Box Height 250": 99,
-    "Attribute": "1673375655977x752951405907542000",
+    Attribute: "1673375655977x752951405907542000",
     "Account Webpage": "1648649408910x887794807113668500",
     "Stroke Width": 2,
-    "Shape": "Rectangle",
+    Shape: "Rectangle",
     "X Coordinate (250)": 96,
     "X Coordinate (960)": 370,
     "Y Coordinate (250)": 197,
     "Y Coordinate (960)": 758,
-    "_id": "1674151425031x842236848750460900"
-  }, {
+    _id: "1674151425031x842236848750460900",
+  },
+  {
     "Corner Roundness": 5,
     "Offset of top (y)": 228,
     "Desktop Screenshot": "1670451431357x388596123285430460",
@@ -37,16 +40,17 @@ var DAS = new Array({
     "X Coordinate (500)": 95,
     "Box Width 250": 30,
     "Box Height 250": 25,
-    "Attribute": "1668510026384x892836158180425700",
+    Attribute: "1668510026384x892836158180425700",
     "Account Webpage": "1648649408910x887794807113668500",
     "Stroke Width": 2,
-    "Shape": "Rectangle",
+    Shape: "Rectangle",
     "X Coordinate (250)": 47,
     "X Coordinate (960)": 182,
     "Y Coordinate (250)": 114,
     "Y Coordinate (960)": 437,
-    "_id": "1674151403457x266515680396836860"
-  }, {
+    _id: "1674151403457x266515680396836860",
+  },
+  {
     "Corner Roundness": 5,
     "Offset of top (y)": 51,
     "Desktop Screenshot": "1672862222970x520878899413092600",
@@ -60,16 +64,17 @@ var DAS = new Array({
     "X Coordinate (500)": 21,
     "Box Width 250": 146,
     "Box Height 250": 77.5,
-    "Attribute": "1668800394117x917004865047887900",
+    Attribute: "1668800394117x917004865047887900",
     "Account Webpage": "1667262889527x858621114244434700",
     "Stroke Width": 2,
-    "Shape": "Rectangle",
+    Shape: "Rectangle",
     "X Coordinate (250)": 10,
     "X Coordinate (960)": 40,
     "Y Coordinate (250)": 25,
     "Y Coordinate (960)": 97,
-    "_id": "1674062600971x785814322671779800"
-  }, {
+    _id: "1674062600971x785814322671779800",
+  },
+  {
     "Corner Roundness": 5,
     "Offset of top (y)": 122,
     "Desktop Screenshot": "1672867270966x627421898112095900",
@@ -83,16 +88,17 @@ var DAS = new Array({
     "X Coordinate (500)": 29,
     "Box Width 250": 30.729166666666668,
     "Box Height 250": 13.541666666666668,
-    "Attribute": "1668800394117x917004865047887900",
+    Attribute: "1668800394117x917004865047887900",
     "Account Webpage": "1672867250672x906510004638974000",
     "Stroke Width": 2,
-    "Shape": "Rectangle",
+    Shape: "Rectangle",
     "X Coordinate (250)": 14,
     "X Coordinate (960)": 57,
     "Y Coordinate (250)": 31,
     "Y Coordinate (960)": 122,
-    "_id": "1674059222456x786064976190898200"
-  }, {
+    _id: "1674059222456x786064976190898200",
+  },
+  {
     "Corner Roundness": 5,
     "Offset of top (y)": 32,
     "Desktop Screenshot": "1672867270966x627421898112095900",
@@ -106,86 +112,117 @@ var DAS = new Array({
     "X Coordinate (500)": 10,
     "Box Width 250": 38.802083333333336,
     "Box Height 250": 17.96875,
-    "Attribute": "1668800394117x917004865047887900",
+    Attribute: "1668800394117x917004865047887900",
     "Account Webpage": "1672867250672x906510004638974000",
     "Stroke Width": 2,
-    "Shape": "Rectangle",
+    Shape: "Rectangle",
     "X Coordinate (250)": 5,
     "X Coordinate (960)": 21,
     "Y Coordinate (250)": 8,
     "Y Coordinate (960)": 32,
-    "_id": "1674046114484x384901095148486660"
-  });
-  //var att = properties.att
-  var att = new Array({
+    _id: "1674046114484x384901095148486660",
+  }
+);
+//var att = properties.att
+var att = new Array(
+  {
     "Latest Volume": 346,
     "Related Webpages": ["1648649408910x887794807113668500"],
     "Created Date": 1673375665144,
-    "Name": "CTA",
-    "Account": "1648562004649x223328173625704450",
+    Name: "CTA",
+    Account: "1648562004649x223328173625704450",
     "Modified Date": 1674151427900,
     "Created By": "1648561974656x439262183960839300",
     "Average Distance from top": 236,
-    "Color": "4",
+    Color: "4",
     "Name Search": "cta",
     "Last Used": 1674151426414,
-    "_id": "1673375655977x752951405907542000"
-  }, {
+    _id: "1673375655977x752951405907542000",
+  },
+  {
     "Latest Volume": 358,
-    "Related Webpages": ["1648649408910x887794807113668500", "1648649415130x341237624746570240"],
+    "Related Webpages": [
+      "1648649408910x887794807113668500",
+      "1648649415130x341237624746570240",
+    ],
     "Created Date": 1668510027553,
-    "Name": "Attribute 3",
-    "Account": "1648562004649x223328173625704450",
+    Name: "Attribute 3",
+    Account: "1648562004649x223328173625704450",
     "Modified Date": 1674151407928,
     "Created By": "1648561974656x439262183960839300",
     "Average Distance from top": 138,
-    "Color": "#ff00ff",
+    Color: "#ff00ff",
     "Name Search": "attribute 3",
     "Last Used": 1674151406474,
-    "_id": "1668510026384x892836158180425700"
-  }, {
+    _id: "1668510026384x892836158180425700",
+  },
+  {
     "Latest Volume": 15442,
-    "Related Webpages": ["1664221313711x475501364962342100", "1664223419885x108644029860312400", "1664221316045x125346515221780750", "1664221313698x632940115935128000", "1672867250672x906510004638974000", "1667262889527x858621114244434700"],
+    "Related Webpages": [
+      "1664221313711x475501364962342100",
+      "1664223419885x108644029860312400",
+      "1664221316045x125346515221780750",
+      "1664221313698x632940115935128000",
+      "1672867250672x906510004638974000",
+      "1667262889527x858621114244434700",
+    ],
     "Created Date": 1668800409315,
-    "Name": "MY attribute",
-    "Account": "1664221229355x903532045837271000",
+    Name: "MY attribute",
+    Account: "1664221229355x903532045837271000",
     "Modified Date": 1674147291058,
     "Created By": "1664221186966x204293830276146340",
     "Average Distance from top": 269,
-    "Color": "#38761d",
+    Color: "#38761d",
     "Name Search": "my attribute",
     "Last Used": 1674147290378,
-    "_id": "1668800394117x917004865047887900"
-  }, {
+    _id: "1668800394117x917004865047887900",
+  },
+  {
     "Latest Volume": 15442,
-    "Related Webpages": ["1664221313711x475501364962342100", "1664223419885x108644029860312400", "1664221316045x125346515221780750", "1664221313698x632940115935128000", "1672867250672x906510004638974000", "1667262889527x858621114244434700"],
+    "Related Webpages": [
+      "1664221313711x475501364962342100",
+      "1664223419885x108644029860312400",
+      "1664221316045x125346515221780750",
+      "1664221313698x632940115935128000",
+      "1672867250672x906510004638974000",
+      "1667262889527x858621114244434700",
+    ],
     "Created Date": 1668800409315,
-    "Name": "MY attribute",
-    "Account": "1664221229355x903532045837271000",
+    Name: "MY attribute",
+    Account: "1664221229355x903532045837271000",
     "Modified Date": 1674147291058,
     "Created By": "1664221186966x204293830276146340",
     "Average Distance from top": 269,
-    "Color": "#38761d",
+    Color: "#38761d",
     "Name Search": "my attribute",
     "Last Used": 1674147290378,
-    "_id": "1668800394117x917004865047887900"
-  }, {
+    _id: "1668800394117x917004865047887900",
+  },
+  {
     "Latest Volume": 15442,
-    "Related Webpages": ["1664221313711x475501364962342100", "1664223419885x108644029860312400", "1664221316045x125346515221780750", "1664221313698x632940115935128000", "1672867250672x906510004638974000", "1667262889527x858621114244434700"],
+    "Related Webpages": [
+      "1664221313711x475501364962342100",
+      "1664223419885x108644029860312400",
+      "1664221316045x125346515221780750",
+      "1664221313698x632940115935128000",
+      "1672867250672x906510004638974000",
+      "1667262889527x858621114244434700",
+    ],
     "Created Date": 1668800409315,
-    "Name": "MY attribute",
-    "Account": "1664221229355x903532045837271000",
+    Name: "MY attribute",
+    Account: "1664221229355x903532045837271000",
     "Modified Date": 1674147291058,
     "Created By": "1664221186966x204293830276146340",
     "Average Distance from top": 269,
-    "Color": "#38761d",
+    Color: "#38761d",
     "Name Search": "my attribute",
     "Last Used": 1674147290378,
-    "_id": "1668800394117x917004865047887900"
-  });
-  //var colors = properties.colors;
-  var colors = new Array("#57d9a3", "ff00ff", "38761d", "38761d", "38761d");
-  //rects array (x,y,width,height,color,id)
+    _id: "1668800394117x917004865047887900",
+  }
+);
+//var colors = properties.colors;
+var colors = new Array("#57d9a3", "ff00ff", "38761d", "38761d", "38761d");
+//rects array (x,y,width,height,color,id)
 var rects = new Array();
 rects[0] = new Array(1, 5, 15, 15, "0xDE3249", 1);
 rects[1] = new Array(100, 150, 250, 250, "0x800080", 2);
@@ -193,4 +230,4 @@ rects[1] = new Array(100, 150, 250, 250, "0x800080", 2);
 var rects2 = new Array();
 rects2[0] = new Array(1, 5, 15, 15, "0x800080", 3);
 rects2[1] = new Array(100, 150, 250, 250, "0xDE3249", 4);
-  export { DAS, att, colors, rects, rects2 };
+export { DAS, att, colors, rects, rects2 };

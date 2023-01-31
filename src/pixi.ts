7062de75b7d5e7b06f829c5727540572d34c9727
csp-syllabus <123014658+csp-syllabus@users.noreply.github.com>
2023-01-31 01:16:57 +0000

    New DRAWING METHOD

--- src/pixi.ts
+++ src/pixi.ts
@@ -3,7 +3,7 @@
 //start C Declare before import
 let startX, startY, endX, endY;
 let isDrawing = false;
-let logging = false;
+let logging = true;
 let logRectEvents = false;
 let loadData = true;
 let logEvents = false;
@@ -42,7 +42,10 @@ import {
   logResize,
   isResizing,
   addResizeHand,
+  ticker
 } from "./events";
+export { reDraw, mainContainer,webpageSprite, rectangles, app};
+import { Graphics } from "pixi.js";
 import { Graphics } from "pixi.js";
 //--begin html container setup, and pixi core element setup
 const imgixBaseURL = `https://d1muf25xaso8hp.cloudfront.net/`;
@@ -136,10 +139,10 @@ function loadDAS(das) {
     //console.log(rect);
     //will need placeholder for color. may need to generate specific
     let createCoord = getStartCoordinates(
-      das["X Coordinate (500)"],
-      das["Y Coordinate (500)"],
-      das["Box Width 250"],
-      das["Box Height 250"]
+      das["X Coordinate (960)"],
+      das["Y Coordinate (960)"],
+      das["Box Width 250"] * 3 ,
+      das["Box Height 250"] * 3
     );
     logging ? console.log("createCoord", createCoord) : null;
 
@@ -147,10 +150,44 @@ function loadDAS(das) {
     if (createCoord.width < 20) return;
     if (createCoord.height < 20) return;
 
-    createRectangle(createCoord, "DE3249", "temp");
+    createRectNew(createCoord, "DE3249", "temp");
     //createRect(das['X Coordinate (500)'], das['Y Coordinate (500)'], das['Box Width 250'], das['Box Height 250'], colors[index], das['_id']);
   });
 }
+
+
+function createRectNew(createCoord, c, id){
+  // Create graphics
+  let currentRectangle = new PIXI.Graphics()
+                      .beginFill(c, 0.5)
+                      // returns initial graphis so we can daizy chaing
+                      .lineStyle({ color: 0x111111, alpha: 0, width: 1 })
+                      // we create rect in orgin for calculation simplification
+                      .drawRect(createCoord.startRectX,
+                        createCoord.startRectY,
+                        createCoord.width,
+                        createCoord.height)
+                      .endFill()
+                      currentRectangle.beginFill(0xffff00, 0.5);
+                      currentRectangle.labelColor = "0x" + c;
+                      currentRectangle.oldColor = "0xFFFF00";
+  
+  // then we move it to final position
+  currentRectangle.name = Math.random().toString(16).substr(2, 8); //id;
+  currentRectangle.intialScale = app.view.width / intialWebpageWidth;
+  currentRectangle.position.copyFrom(new PIXI.Point(100,100))
+  addLabel(currentRectangle);
+  app.stage.addChild(currentRectangle)
+  // make it hoverable
+  currentRectangle.interactive = true;
+  currentRectangle
+      .on('pointerover', onRectangleOver)
+      .on('pointerout', onRectangleOut);
+  // remove it from current ceration and chose new color for next one
+  currentRectangle = null
+  rectangles.push(currentRectangle);
+}
+
 function createRectangle(createCoord, c, id) {
   // Create a new rectangle graphic using the calculated dimensions
   isDrawing = false;
@@ -207,20 +244,17 @@ function loadImage(im) {
 //adds a label PLACEHOLDER
 function addLabel(rect) {
   const label = new PIXI.Text(rect.name, {
-    fontFamily: "Arial",
+    fontFamily: "Inter",
     fontSize: 24,
-    fill: 0x000000,
+    fill: 0x000000
   });
-  label.position.set(
-    rect.getBounds().x + 20 - mainContainer.position.x,
-    rect.getBounds().y + 20 - mainContainer.position.y
-  );
+  label.position.set(label.position.x + 10, label.position.y + 10);
   logging ? console.log("label", rect, rect.getBounds()) : null;
   rect.addChild(label);
 }
 //Listeners
-
-mainContainer.on("pointerup", (event) => {
+/*
+app.stage.on("pointerup", (event) => {
   logEvents
     ? console.log("pointerup", event.target.name, "start", startX, startY)
     : null;
@@ -289,7 +323,7 @@ mainContainer.on("pointerup", (event) => {
     : null;
 });
 
-mainContainer.on("pointerdown", (event) => {
+app.stage.on("pointerdown", (event) => {
   logDrag
     ? console.log(
         "pointerdown-Event,target,target-parent",
@@ -323,7 +357,7 @@ mainContainer.on("pointerdown", (event) => {
     return;
   }
 });
-/*
+
 mainContainer.on("mouseup", (event) => {
   if (event.target.name == "dragHandle") {
     isDrawing = false;
@@ -605,4 +639,340 @@ function reorderRectangles(rectangles) {
     return a.sortSize - b.sortSize;
   });
 }
+
+function reDraw() {
+  mainContainer.removeChildren();
+  // Add the image back to the stage
+  mainContainer.addChild(webpageSprite);
+  // Add all previously added rectangles back to the stage
+  rectangles.forEach((r) => mainContainer.addChild(r));
+}
 loadData ? loadDAS(DAS) : null;
+//ticker.start();
+
+
+/////////////////////NEW DRAWING
+// Input modes for input
+let InputModeEnum = {
+  create: 1,
+  select: 2,
+  scale: 3,
+  move: 4
+}
+
+// Create our application instance
+/*
+var app = new PIXI.Application({
+  width: window.innerWidth,
+  height: window.innerHeight,
+  backgroundColor: 0x2c3e50
+});
+
+document.body.appendChild(app.view);
+// Make main canvas interactable to tract events
+*/
+app.stage.interactive = true;
+app.stage.hitArea = app.screen;
+
+// Load textures for edit
+const hrefMove = "https://s3.amazonaws.com/appforest_uf/d110/f1674669224748x768134644407078900/drag_indicator_FILL0_wght400_GRAD0_opsz48.png"
+const hrefScale = "https://s3.amazonaws.com/appforest_uf/d110/f1674585363384x114691738198125620/drag-handle-corner.png?ignore_imgix=true"
+const textureMove = PIXI.Texture.from(hrefMove);
+const textureScale = PIXI.Texture.from(hrefScale);
+
+// Start position of events
+let startPosition = null
+// Random color for next rect drawing
+let currentColor =   0xffff00;
+
+// Current edited or created rectangle
+let currentRectangle = null;
+// Set mode for whole input (by default we create rectangles)
+let inputMode = InputModeEnum.create;
+
+// Pointer to current active controll (Move or Scale icon)
+let dragController = null;
+// Active shown controls (can be shown both or one hovered / active)
+let controls = [];
+
+// Function that generates first rect (shows all steps needed for that)
+function generateFirstRect(){
+  // Create graphics
+  currentRectangle = new PIXI.Graphics()
+                      .beginFill(currentColor, 0.5)
+                      // returns initial graphis so we can daizy chaing
+                      .lineStyle({ color: 0x111111, alpha: 0, width: 1 })
+                      // we create rect in orgin for calculation simplification
+                      .drawRect(0,0,100,100)
+                      .endFill()
+  // then we move it to final position
+  currentRectangle.labelColor = "0x";
+  currentRectangle.oldColor = "0xFFFF00";
+  currentRectangle.name = "0xFFFF00";
+  currentRectangle.position.copyFrom(new PIXI.Point(100,100))
+  addLabel(currentRectangle);
+  app.stage.addChild(currentRectangle)
+  // make it hoverable
+  currentRectangle.interactive = true;
+  currentRectangle
+      .on('pointerover', onRectangleOver)
+      .on('pointerout', onRectangleOut);
+  // remove it from current ceration and chose new color for next one
+  currentRectangle = null
+  currentColor = Math.random() * 0xFFFFFF
+}
+//generateFirstRect()
+
+app.stage.on('pointerdown', (e) => {
+  // Initiate rect creation
+  console.log(inputMode)
+  if(inputMode == InputModeEnum.create)
+      startPosition = new PIXI.Point().copyFrom(e.data.global)
+      logging ? console.log("pointerdown",startPosition) : null;
+});
+
+app.stage.on('pointermove', (e) => {
+  // Do this routine only if in create mode and have started creation
+  // this event triggers all the time but we stop it by not providing start postition when cursor not pressed
+  if(inputMode == InputModeEnum.create && startPosition){
+      // get new global position from event
+      let currentPosition = e.data.global;
+      let {start, size} = getStartAndSize(currentPosition, startPosition)
+      if(size.x > 5 && size.y > 5){
+          if(!currentRectangle){
+             currentRectangle = new PIXI.Graphics()
+                  .beginFill(currentColor, .5)
+                  .lineStyle({ color: 0xFFFF00, alpha: 0.5, width: 1 })
+                  .drawRect(0,0,size.x,size.y)
+                  .endFill()
+                  currentRectangle.labelColor = "0x";
+                  currentRectangle.oldColor = "0xFFFF00";
+                  currentRectangle.name = "0xFFFF00";
+                  currentRectangle.position.copyFrom(start)
+                  addLabel(currentRectangle);
+              app.stage.addChild(currentRectangle)
+          }
+          else {
+              scaleRectB(currentRectangle,currentPosition)
+          }
+      } else {
+          if(currentRectangle){
+              app.stage.removeChild(currentRectangle);
+              currentRectangle = null
+          }
+      }
+
+  }
+});
+
+app.stage.on('pointerup', (e) => {
+  // Wrap up rect creation
+  startPosition = null
+  if(currentRectangle && currentRectangle.interactive == false){
+      currentRectangle.interactive = true;
+      currentRectangle
+      // Mouse & touch events are normalized into
+      // the pointer* events for handling different
+      // Rectangle events.
+          .on('pointerover', onRectangleOver)
+          .on('pointerout', onRectangleOut);
+  }
+  currentRectangle = null
+  currentColor = Math.random() * 0xFFFFFF
+  onDragEndNew()
+});
+
+function controlOver(){
+  this.isOver = true;
+}
+function controlOut(){
+  this.isOver = false;
+  removeIfUnused(this)
+}
+function onRectangleOver() {
+  // Do not hover rectangle if we are moving
+  if(inputMode == InputModeEnum.move || inputMode == InputModeEnum.scale){
+      return
+  }
+  // Do not hover rectangle if we creating
+  if(inputMode == InputModeEnum.create && startPosition){
+      return
+  }
+  console.log("Over")
+  this.isOver = true;
+  inputMode = InputModeEnum.select
+  // set current hovered rect to be on the top
+  bringToFront(this)
+  // Create button move
+  const buttonMove = new PIXI.Sprite(textureMove);
+  // Make make its codinate to be at center of sprite
+  // Usualy it is in top left corner but this suit su better
+  buttonMove.anchor.set(0.5);
+  // Set controll top right
+  buttonMove.x = this.x + this.width - 18;
+  buttonMove.y = this.y + 23;
+
+  // make the buttonMove interactive...
+  buttonMove.interactive = true;
+  buttonMove.cursor = 'pointer';
+  // Add coresponding events
+  // pass alternative this object to pointerdown event
+  // so we know which controll is pressed
+  buttonMove.on('pointerover', controlOver)
+          .on('pointerout', controlOut)
+          .on('pointerdown', onDragStartNew, {
+              controller: buttonMove,
+              edit: this,
+              mode: InputModeEnum.move
+              });
+  app.stage.addChild(buttonMove);
+  // add to controls queue
+  controls.push(buttonMove);
+
+  const buttonScale = new PIXI.Sprite(textureScale);
+  // same as for move but on bottom right corner
+  buttonScale.anchor.set(0.5);
+  buttonScale.x = this.x + this.width - 20;
+  buttonScale.y = this.y + this.height - 20;
+
+  // make the buttonScale interactive...
+  buttonScale.interactive = true;
+  buttonScale.cursor = 'pointer';
+  buttonScale.on('pointerover', controlOver)
+          .on('pointerout', controlOut)
+          .on('pointerdown', onDragStartNew, {
+              controller: buttonScale,
+              edit: this,
+              mode: InputModeEnum.scale
+              });
+
+  app.stage.addChild(buttonScale);
+  controls.push(buttonScale);
+}
+function cleanupcontrols(){
+  controls = controls.filter(el=>el.parent)
+  if(controls.length == 0){
+      onDragEndNew()
+  }
+}
+
+function removeIfUnused(controll){
+  // Give time to PIXI to go trough events and add event on end
+      setTimeout(()=>{ 
+          // By this time isOver is updated so if we went from
+          // square to conntroll controll.isOver become true
+          // if we are draging controll and went out of it
+          // we dont remove it because it is still used
+          if(!controll.isOver && controll !== dragController){
+              app.stage.removeChild(controll);
+              cleanupcontrols()
+          }
+      },0)
+}
+// Event is triggered when we move mouse out of rectangle
+function onRectangleOut() {
+  this.isOver = false;
+  console.log("OUT");
+  controls.forEach(el=>removeIfUnused(el))
+}
+// Normalise start and size of square
+// so we allways have top left corenr as start and size is allways positive
+function getStartAndSize(pointA, pointB){
+  let deltaX = pointB.x - pointA.x;
+  let deltaY = pointB.y - pointA.y;
+  let absDeltaX = Math.abs(deltaX)
+  let absDeltaY = Math.abs(deltaY)
+  let startX = deltaX > 0 ? pointA.x : pointB.x
+  let startY = deltaY > 0 ? pointA.y : pointB.y
+  return { start: new PIXI.Point(startX, startY),
+      size: new PIXI.Point(absDeltaX, absDeltaY)
+      }
+}
+function scaleRectB(resizeRectange, currentPosition){
+  let {start, size} = getStartAndSize(startPosition, currentPosition)
+  if(size.x<5 || size.y<5) return
+  // When we scale rect we have to give it new cordinates so we redraw it
+  // in case of sprite we would do this a bit differently with scale property,
+  // for simple geometry this is better solution because scale propagates to children
+  resizeRectange.clear()
+  resizeRectange.position.copyFrom(start)
+  resizeRectange.beginFill(currentColor, .5)
+      .lineStyle({ color: 0x111111, alpha: 0.5, width: 1 })
+      .drawRect(0,0,size.x,size.y)
+      .endFill()
+}
+
+
+function scaleRect(resizeRectange, dragController ){
+  //currentPosition){
+  let {start, size} = getStartAndSize(startPosition, dragController.position)
+  if(size.x<5 || size.y<5) return
+  // When we scale rect we have to give it new cordinates so we redraw it
+  // in case of sprite we would do this a bit differently with scale property,
+  // for simple geometry this is better solution because scale propagates to children
+
+  let startPositionController = new PIXI.Point(dragController.position.x - 20, dragController.position.y - 20);
+  resizeRectange.clear()
+  resizeRectange.position.copyFrom(start)
+  resizeRectange.beginFill(currentColor, .5)
+      .lineStyle({ color: 0x111111, alpha: 0.5, width: 1 })
+      .drawRect(0,0,size.x,size.y)
+      .endFill()
+
+      dragController.position.copyFrom(startPositionController)
+}
+
+function moveRect(resizeRectange, dragController){
+  // Move control is on right side
+  // and our rect is anchored on the left we substact width of rect
+  let startPosition = new PIXI.Point(dragController.position.x - resizeRectange.width, dragController.position.y);
+  let startPositionController = new PIXI.Point(dragController.position.x - 18, dragController.position.y + 23);
+  // we just move the start position
+  resizeRectange.position.copyFrom(startPosition)
+  dragController.position.copyFrom(startPositionController)
+}
+
+function onDragMoveNew(event) {
+  if (dragController) {
+      // move control icon (move or scale icon)
+      dragController.parent.toLocal(event.data.global, null, dragController.position);
+      if(inputMode == InputModeEnum.scale){
+          // handle rect scale
+          scaleRect(currentRectangle,dragController)
+      }
+      if(inputMode == InputModeEnum.move){
+          // handle rect move
+          moveRect(currentRectangle,dragController)
+      }
+  }
+}
+
+function onDragStartNew() {
+  // start drag of controller
+  // and set parameters
+  this.controller.alpha = 0.5;
+  dragController = this.controller;
+  currentRectangle = this.edit
+  startPosition = new PIXI.Point().copyFrom(this.edit.position)
+  inputMode = this.mode
+  app.stage.on('pointermove', onDragMoveNew);
+}
+
+function onDragEndNew() {
+  // stop drag, remove parameters
+  // return input mode to default
+  if(dragController){
+      app.stage.off('pointermove', onDragMoveNew);
+      dragController.alpha = 1;
+      currentRectangle = null;
+      dragController = null;
+  }
+  startPosition = null;
+  inputMode = InputModeEnum.create;
+}
+
+app.stage.on('pointerupoutside', onDragEndNew);
+
+console.log("reload")
+
+function bringToFront(sprite) {var sprite = (typeof(sprite) != "undefined") ? sprite.target || sprite : this;var parent = sprite.parent || {"children": false};if (parent.children) {    for (var keyIndex in sprite.parent.children) {         if (sprite.parent.children[keyIndex] === sprite) {            sprite.parent.children.splice(keyIndex, 1);            break;        }    }    parent.children.push(sprite);}}

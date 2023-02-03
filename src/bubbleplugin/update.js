783292fa474fa0ff94dde891b1b7c8afaf9e5760
Steven Junio <Steven.junio91@gmail.com>
2023-02-02 18:31:49 -0800

    Fix blank screen, but busted scrollbar after swap

--- src/bubbleplugin/update.js
+++ src/bubbleplugin/update.js
@@ -184,6 +184,21 @@ ATT
     ////////////// NOTE TO STEVEN: wrapped this for various reasons, maybe we should move some of these like mainContainer to instance everywhere, not sure
     if (instance.data.start) {
 
+        // Select the target element
+
+
+        // Create the IntersectionObserver
+        instance.data.observer = new IntersectionObserver((entries, observer) => {
+            // Check if the target element is intersecting with the viewport
+            if (entries[0].isIntersecting) {
+                console.log('Element has come into view');
+            }
+        });
+
+        // Start observing the target element
+        instance.data.observer.observe(instance.canvas);
+
+
         //listen for resizes of the element
 
         instance.data.mainElementObserver = new ResizeObserver((entries) => {
@@ -240,159 +255,163 @@ ATT
 
     }
     if (properties.webpage_screenshot) {
+        //load the webpage and rects with a delay, because Bubble doesn't load the container at the same time this is visible
+        setTimeout(() => {
+            screenshot = PIXI.Texture.fromURL(
+                `${imgixBaseURL}/${properties.webpage_screenshot}?w=${1000}`
+            ).then((texture) => {
+                mainContainer.removeChildren()
 
-        screenshot = PIXI.Texture.fromURL(
-            `${imgixBaseURL}/${properties.webpage_screenshot}?w=${1000}`
-        ).then((texture) => {
-            mainContainer.removeChildren()
-
-            texture.baseTexture.scaleMode = PIXI.SCALE_MODES.LINEAR;
-            instance.data.webpageSprite = PIXI.Sprite.from(texture);
-            webpageSprite = instance.data.webpageSprite;
-            instance.data.intialWebpageWidth = webpageSprite.width;
-            console.log("reset the intialwebpage width", instance.data.intialWebpageWidth)
-            intialWebpageHeight = webpageSprite.height;
-            webpageSprite.name = "webpage";
-            webpageSprite.intialWidth = webpageSprite.width;
-            mainContainer.addChild(webpageSprite);
-            mainContainer.interactive = true;
-            webpageSprite.scale.set(instance.data.app.view.width / webpageSprite.width);
-            intialCanvasWidth = instance.data.app.view.width;
-            intialCanvasHeight = instance.data.app.view.height;
-            intialScale = intialCanvasWidth / instance.data.intialWebpageWidth;
-            webpageSprite.intialScale = instance.data.app.view.width / webpageSprite.width;
-
-            //c listeners
-            if (!instance.data.addedMainContainerEventListeners) {
-                mainContainer.on('pointerdown', (e) => {
-                    // Initiate rect creation
-                    if (instance.data.inputMode == instance.data.InputModeEnum.create) {
-                        instance.data.startPosition = new PIXI.Point().copyFrom(e.global)
-                        instance.data.logging ? console.log("pointerdown", instance.data.startPosition) : null;
-                        instance.publishState("currently_selected_drawing", '')
-
-                    }
-                    if (instance.data.inputMode == instance.data.InputModeEnum.select) {
-                        //PLACEHOLDER for select function
-                        instance.data.selectRect(e.target);
-                    }
-                });
-                mainContainer.on('pointermove', (e) => {
-                    // Do this routine only if in create mode and have started creation
-                    // this event triggers all the time but we stop it by not providing start postition when cursor not pressed
-                    if (instance.data.inputMode == instance.data.InputModeEnum.create && instance.data.startPosition) {
-                        // get new global position from event
-                        let currentPosition = e.global;
-                        let {
-                            start,
-                            size
-                        } = instance.data.getStartAndSize(currentPosition, instance.data.startPosition, "draw")
-                        if (size.x > 5 && size.y > 5) {
-                            if (!instance.data.currentRectangle) {
-                                instance.data.currentRectangle = new PIXI.Graphics().beginFill("0x" + instance.data.highlightColor, .5)
-                                    .lineStyle({
-                                        color: "0x" + instance.data.highlightColor,
-                                        alpha: 0.5,
-                                        width: 1
-                                    }).drawRect(0, 0, size.x, size.y).endFill()
-                                instance.data.currentRectangle.labelColor = "0x" + instance.data.highlightColor;
-                                instance.data.currentRectangle.oldColor = instance.data.highlightColor;
-                                instance.data.currentRectangle.name = "";
-                                instance.data.currentRectangle.intialScale = instance.data.app.view.width / instance.data.intialWebpageWidth;
-
-                                instance.data.currentRectangle.position.copyFrom(start)
-                                instance.data.addLabel(instance.data.currentRectangle);
-                                mainContainer.addChild(instance.data.currentRectangle)
-                            } else {
-                                instance.data.scaleRectB(instance.data.currentRectangle, currentPosition)
-                            }
-                        } else {
-                            if (instance.data.currentRectangle) {
-                                mainContainer.removeChild(instance.data.currentRectangle);
-                                instance.data.currentRectangle = null
-                            }
-                        }
-                    }
-                });
-                mainContainer.on('pointerup', (e) => {
-                    // Wrap up rect creation
-                    instance.data.startPosition = null
-                    if (instance.data.currentRectangle && instance.data.currentRectangle.interactive == false) {
-                        instance.data.currentRectangle.interactive = true;
-                        instance.data.currentRectangle
-                            // Mouse & touch events are normalized into
-                            // the pointer* events for handling different
-                            // Rectangle events.
-                            .on('pointerover', instance.data.onRectangleOver).on('pointerout', instance.data.onRectangleOut);
-
-                        instance.data.currentRectangle.initialScale = instance.data.app.view.width / instance.data.intialWebpageWidth;
-
-
-
-                        let rectData = [
-                            instance.data.currentRectangle.x.toString(),
-                            instance.data.currentRectangle.y.toString(),
-                            instance.data.currentRectangle.width.toString(),
-                            instance.data.currentRectangle.height.toString(),
-                            instance.data.currentRectangle.name,
-                            instance.data.currentRectangle.labelColor.toString(),
-                            instance.data.currentRectangle.oldColor.toString(),
-                            instance.data.currentRectangle.initialScale.toString()
-                        ]
-                        //create the data directly via the API
-                        let headersList = {
-                            "Accept": "*/*",
-                        }
-
-                        let bodyContent = new FormData();
-                        bodyContent.append("x", rectData[0]);
-                        bodyContent.append("y", rectData[1]);
-                        bodyContent.append("width", rectData[2]);
-                        bodyContent.append("height", rectData[3]);
-                        bodyContent.append("initial_drawn_scale", rectData[7]);
-                        bodyContent.append("account_webpage", properties.account_webpage);
-
-                        fetch("https://app.syllabus.io/version-steven-canvas-implementat/api/1.1/wf/create-new-drawn-label", {
-                            method: "POST",
-                            body: bodyContent,
-                            headers: headersList
-                        }).then(response => response.json())
-                            .then(result => {
-                                let newID = result.response.drawn_attribute_snippet._id;
-
-                                console.log(result.response.drawn_attribute_snippet);
-
-
-                                instance.publishState(`recently_created_drawing_data`, rectData)
-                                instance.publishState(`recently_created_drawn_label`, newID)
-                                setTimeout(() => {
-                                    instance.triggerEvent(`drawn_label_created`)
-
-                                }, 200)
-
-                            })
-
-
-
-                    }
-                    instance.data.currentRectangle = null
-                    instance.data.onDragEndNew()
-                });
-                //load our data
-                mainContainer.on('pointerupoutside', instance.data.onDragEndNew);
+                texture.baseTexture.scaleMode = PIXI.SCALE_MODES.LINEAR;
+                instance.data.webpageSprite = PIXI.Sprite.from(texture);
+                webpageSprite = instance.data.webpageSprite;
+                instance.data.intialWebpageWidth = webpageSprite.width;
+                intialWebpageHeight = webpageSprite.height;
+                webpageSprite.name = "webpage";
+                webpageSprite.intialWidth = webpageSprite.width;
+                mainContainer.addChild(webpageSprite);
                 mainContainer.interactive = true;
-                mainContainer.hitArea = mainContainer.screen;
-                instance.data.addedMainContainerEventListeners = true;
-            }
-            console.log(`loadData?`, instance.data.loadData)
-            instance.data.loadData ? instance.data.loadDAS(instance.data.dasOrigin) : null;
-            console.log(`the dasorigin is `, instance.data.dasOrigin)
-            if (instance.data.createdScrollBar == false) {
+                webpageSprite.scale.set(instance.data.app.view.width / webpageSprite.width);
+                intialCanvasWidth = instance.data.app.view.width;
+                intialCanvasHeight = instance.data.app.view.height;
+                intialScale = intialCanvasWidth / instance.data.intialWebpageWidth;
+                webpageSprite.intialScale = instance.data.app.view.width / webpageSprite.width;
+
+                //c listeners
+                if (!instance.data.addedMainContainerEventListeners) {
+                    mainContainer.on('pointerdown', (e) => {
+                        // Initiate rect creation
+                        if (instance.data.inputMode == instance.data.InputModeEnum.create) {
+                            instance.data.startPosition = new PIXI.Point().copyFrom(e.global)
+                            instance.data.logging ? console.log("pointerdown", instance.data.startPosition) : null;
+                            instance.publishState("currently_selected_drawing", '')
+
+                        }
+                        if (instance.data.inputMode == instance.data.InputModeEnum.select) {
+                            //PLACEHOLDER for select function
+                            instance.data.selectRect(e.target);
+                        }
+                    });
+                    mainContainer.on('pointermove', (e) => {
+                        // Do this routine only if in create mode and have started creation
+                        // this event triggers all the time but we stop it by not providing start postition when cursor not pressed
+                        if (instance.data.inputMode == instance.data.InputModeEnum.create && instance.data.startPosition) {
+                            // get new global position from event
+                            let currentPosition = e.global;
+                            let {
+                                start,
+                                size
+                            } = instance.data.getStartAndSize(currentPosition, instance.data.startPosition, "draw")
+                            if (size.x > 5 && size.y > 5) {
+                                if (!instance.data.currentRectangle) {
+                                    instance.data.currentRectangle = new PIXI.Graphics().beginFill("0x" + instance.data.highlightColor, .5)
+                                        .lineStyle({
+                                            color: "0x" + instance.data.highlightColor,
+                                            alpha: 0.5,
+                                            width: 1
+                                        }).drawRect(0, 0, size.x, size.y).endFill()
+                                    instance.data.currentRectangle.labelColor = "0x" + instance.data.highlightColor;
+                                    instance.data.currentRectangle.oldColor = instance.data.highlightColor;
+                                    instance.data.currentRectangle.name = "";
+                                    instance.data.currentRectangle.intialScale = instance.data.app.view.width / instance.data.intialWebpageWidth;
+
+                                    instance.data.currentRectangle.position.copyFrom(start)
+                                    instance.data.addLabel(instance.data.currentRectangle);
+                                    mainContainer.addChild(instance.data.currentRectangle)
+                                } else {
+                                    instance.data.scaleRectB(instance.data.currentRectangle, currentPosition)
+                                }
+                            } else {
+                                if (instance.data.currentRectangle) {
+                                    mainContainer.removeChild(instance.data.currentRectangle);
+                                    instance.data.currentRectangle = null
+                                }
+                            }
+                        }
+                    });
+                    mainContainer.on('pointerup', (e) => {
+                        // Wrap up rect creation
+                        instance.data.startPosition = null
+                        if (instance.data.currentRectangle && instance.data.currentRectangle.interactive == false) {
+                            instance.data.currentRectangle.interactive = true;
+                            instance.data.currentRectangle
+                                // Mouse & touch events are normalized into
+                                // the pointer* events for handling different
+                                // Rectangle events.
+                                .on('pointerover', instance.data.onRectangleOver).on('pointerout', instance.data.onRectangleOut);
+
+                            instance.data.currentRectangle.initialScale = instance.data.app.view.width / instance.data.intialWebpageWidth;
+
+
+
+                            let rectData = [
+                                instance.data.currentRectangle.x.toString(),
+                                instance.data.currentRectangle.y.toString(),
+                                instance.data.currentRectangle.width.toString(),
+                                instance.data.currentRectangle.height.toString(),
+                                instance.data.currentRectangle.name,
+                                instance.data.currentRectangle.labelColor.toString(),
+                                instance.data.currentRectangle.oldColor.toString(),
+                                instance.data.currentRectangle.initialScale.toString()
+                            ]
+                            //create the data directly via the API
+                            let headersList = {
+                                "Accept": "*/*",
+                            }
+
+                            let bodyContent = new FormData();
+                            bodyContent.append("x", rectData[0]);
+                            bodyContent.append("y", rectData[1]);
+                            bodyContent.append("width", rectData[2]);
+                            bodyContent.append("height", rectData[3]);
+                            bodyContent.append("initial_drawn_scale", rectData[7]);
+                            bodyContent.append("account_webpage", properties.account_webpage);
+
+                            fetch("https://app.syllabus.io/version-steven-canvas-implementat/api/1.1/wf/create-new-drawn-label", {
+                                method: "POST",
+                                body: bodyContent,
+                                headers: headersList
+                            }).then(response => response.json())
+                                .then(result => {
+                                    let newID = result.response.drawn_attribute_snippet._id;
+
+                                    console.log(result.response.drawn_attribute_snippet);
+
+
+                                    instance.publishState(`recently_created_drawing_data`, rectData)
+                                    instance.publishState(`recently_created_drawn_label`, newID)
+                                    setTimeout(() => {
+                                        instance.triggerEvent(`drawn_label_created`)
+
+                                    }, 200)
+
+                                })
+
+
+
+                        }
+                        instance.data.currentRectangle = null
+                        instance.data.onDragEndNew()
+                    });
+                    //load our data
+                    mainContainer.on('pointerupoutside', instance.data.onDragEndNew);
+                    mainContainer.interactive = true;
+                    mainContainer.hitArea = mainContainer.screen;
+                    instance.data.addedMainContainerEventListeners = true;
+                }
+                instance.data.loadData ? instance.data.loadDAS(instance.data.dasOrigin) : null;
+
+                if (typeof instance.data.scrollBar.destroy === "function") {
+                    instance.data.scrollBar.destroy();
+                }
                 instance.data.scrollBar = instance.data.createScrollBar(instance.data.mainContainer, instance.data.app, instance.data.ele);
                 instance.data.createdScrollBar = true;
-            }
 
-            instance.data.addedScreenshot = true
-        });
+                setTimeout(() => {
+                    instance.data.app.resize();
+                }, 100)
+
+            });
+        }, 200)
     }
 }
